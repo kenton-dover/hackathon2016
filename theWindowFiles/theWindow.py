a81c08dd1bbfe40b136796aa4a9a912144fb93c8
ahearnkyle <ahearnkyle01@gmail.com>
2016-04-02 20:40:37 -0400

    made the back ground a grassy area and change some colors
    just keeping it up to data.  the main things i changed are above

--- theWindowFiles/theWindow.py
+++ theWindowFiles/theWindow.py
@@ -5,7 +5,9 @@ pygame.init();
 dimensions = ( SCREEN_WIDTH , SCREEN_HEIGHT )
 color =   ( colorRed , colorBlue , colorGreen )
 screen = pygame.display.set_mode(dimensions)
-background_image = pygame.image.load("ground.png").convert()
+picture = pygame.image.load("flippyboard.png")
+picture = pygame.transform.scale(picture,(SCREEN_WIDTH,SCREEN_HEIGHT))
+background_image = picture.convert()
 going = True
 clock = pygame.time.Clock()
 #inital position
@@ -24,23 +26,23 @@ while going == True:
         y -= 10
         theWeapon = pygame.Rect(x+50,y,5,-50)
         if pressed[pygame.K_SPACE]:
-            pygame.draw.rect(screen, (0, 128, 255), theWeapon)#create rectangle in the window
+            pygame.draw.rect(screen, (0, 0, 0), theWeapon)#create rectangle in the window
     elif pressed[pygame.K_s]and (y < SCREEN_HEIGHT - rectLength):# bounds = screen height - rectangle height 
         y += 10
         theWeapon = pygame.Rect(x,y,5,100)
         if pressed[pygame.K_SPACE]:
-            pygame.draw.rect(screen, (0, 128, 255), theWeapon)#create rectangle in the window
+            pygame.draw.rect(screen, (0, 0, 0), theWeapon)#create rectangle in the window
     elif pressed[pygame.K_a] and (x > 0):
         x -= 10
         theWeapon = pygame.Rect(x-50,y,100,5)
         if pressed[pygame.K_SPACE]:
-            pygame.draw.rect(screen, (0, 128, 255), theWeapon)#create rectangle in the window
+            pygame.draw.rect(screen, (0, 0, 0), theWeapon)#create rectangle in the window
         theWeapon = pygame.Rect(x,y+50,100,5)
     elif pressed[pygame.K_d]and (x < SCREEN_WIDTH - rectHeight):
         x += 10
         theWeapon = pygame.Rect(x+20,y,100,5)
         if pressed[pygame.K_SPACE]:
-            pygame.draw.rect(screen, (0, 128, 255), theWeapon)#create rectangle in the window
+            pygame.draw.rect(screen, (0, 0, 0), theWeapon)#create rectangle in the window
     thePerson = pygame.Rect(x, y, rectHeight, rectLength)
     
     pygame.draw.rect(screen, (0, 128, 255), thePerson)#create rectangle in the window
