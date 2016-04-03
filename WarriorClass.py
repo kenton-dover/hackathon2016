c108669bd9a3991bce8311e28bc84fc9a6d02dbe
MayJacob <jakemay46@yahoo.com>
2016-04-03 00:52:51 -0400

    Real animation with sprite
    contains the warrior class file with ability to handle sprite change
    depending on direction

--- WarriorClass.py
+++ WarriorClass.py
@@ -5,16 +5,29 @@ import math
 import constants
 
 class warriorCharacter:
-    characterSprite = pygame.image.load("Knight_Down_Still.png")
+    downSprite = pygame.image.load("Knight_Down_Still.png")
+    upSprite = pygame.image.load("Knight_Up_Still.png")
+    leftSprite = pygame.image.load("Knight_Left_Still.png")
+    rightSprite = pygame.image.load("Knight_Right_Still.png")
     def __init__(self):
         self.x = random.randint(0, constants.SCREEN_WIDTH)
         self.y = random.randint(0, constants.SCREEN_HEIGHT)
+        self.yDirection = 0
+        self.xDirection = 0
 
     
 
     
     def draw(self, screen):
-        newPosition = warriorCharacter.characterSprite
+        if self.yDirection == 0 and self.xDirection == 0:
+            newPosition = warriorCharacter.downSprite
+        elif self.yDirection == 1 and self.xDirection == 0:
+            newPosition = warriorCharacter.upSprite
+        elif self.yDirection == 0 and self.xDirection == 1:
+            newPosition = warriorCharacter.leftSprite
+        elif self.yDirection == 1 and self.xDirection == 1:
+            newPosition = warriorCharacter.rightSprite
+        
         newPosition.set_colorkey((255, 255, 255))
         rect = newPosition.get_rect()
         rect.centerx = self.x
--- theWindow.py
+++ theWindow.py
@@ -25,19 +25,26 @@ class goesInClientLater:
             pressed = pygame.key.get_pressed()
             if pressed[pygame.K_w] and (y > 0):# added boundes for rectangle
                 character.y -= 10
+                character.yDirection = 1
+                character.xDirection = 0
 
             elif pressed[pygame.K_s]and (y < 480 - 60):# bounds = screen height - rectangle height 
                 character.y += 10
+                character.yDirection = 0
+                character.xDirection = 0
                 
             elif pressed[pygame.K_a] and (x > 0):
                 character.x -= 10
-                
+                character.yDirection = 0
+                character.xDirection = 1
             elif pressed[pygame.K_d]and (x < 640 - 60):
-                character.x += 10                
+                character.x += 10
+                character.yDirection = 1
+                character.xDirection = 1
     
             pygame.display.flip()#updates the window to every change that happens
     
-            clock.tick(20)
+            clock.tick(60)
 
 
         pygame.quit()
