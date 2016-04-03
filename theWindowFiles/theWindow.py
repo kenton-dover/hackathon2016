4a4a4678fc10fe9ee37ebd6a88577c95b0409893
Sam Homans <samuelh238@gmail.com>
2016-04-02 20:09:22 -0400

    combind code

--- theWindowFiles/theWindow.py
+++ theWindowFiles/theWindow.py
@@ -1,27 +1,52 @@
 import pygame
+import constants
+from constants import *
 pygame.init();
-dimensions = ( 640 , 480 )
-color =   ( 255 , 255 , 255 )
+dimensions = ( SCREEN_WIDTH , SCREEN_HEIGHT )
+color =   ( colorRed , colorBlue , colorGreen )
 screen = pygame.display.set_mode(dimensions)
 background_image = pygame.image.load("ground.png").convert()
 going = True
 clock = pygame.time.Clock()
+#inital position
 x = 30
 y = 30
+
 
 while going == True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             going = False
-    screen.fill(color) # ORDER MATTERS : should go Color, background, then objects
+    screen.fill(color)
     screen.blit(background_image, [0, 0])
     pressed = pygame.key.get_pressed()
-    if pressed[pygame.K_UP] and (y > 0): y -= 10 # added boundes for rectangle
-    if pressed[pygame.K_DOWN]and (y < 480 - 60): y += 10 # bounds = screen height - rectangle height 
-    if pressed[pygame.K_LEFT] and (x > 0): x -= 10
-    if pressed[pygame.K_RIGHT]and (x < 640 - 60): x += 10
-    pygame.draw.rect(screen, (0, 128, 255), pygame.Rect(x, y, 60, 60))
-    pygame.display.flip()
+    if pressed[pygame.K_w] and (y > 0):# added boundes for rectangle
+        y -= 10
+        theWeapon = pygame.Rect(x+50,y,5,-50)
+        if pressed[pygame.K_SPACE]:
+            pygame.draw.rect(screen, (0, 128, 255), theWeapon)#create rectangle in the window
+    if pressed[pygame.K_s]and (y < SCREEN_HEIGHT - rectLength):# bounds = screen height - rectangle height 
+        y += 10
+        theWeapon = pygame.Rect(x,y,5,100)
+        if pressed[pygame.K_SPACE]:
+            pygame.draw.rect(screen, (0, 128, 255), theWeapon)#create rectangle in the window
+    if pressed[pygame.K_a] and (x > 0):
+        x -= 10
+        theWeapon = pygame.Rect(x-50,y,100,5)
+        if pressed[pygame.K_SPACE]:
+            pygame.draw.rect(screen, (0, 128, 255), theWeapon)#create rectangle in the window
+        theWeapon = pygame.Rect(x,y+50,100,5)
+    if pressed[pygame.K_d]and (x < SCREEN_WIDTH - rectHeight):
+        x += 10
+        theWeapon = pygame.Rect(x+20,y,100,5)
+        if pressed[pygame.K_SPACE]:
+            pygame.draw.rect(screen, (0, 128, 255), theWeapon)#create rectangle in the window
+    thePerson = pygame.Rect(x, y, rectHeight, rectLength)
+    
+    pygame.draw.rect(screen, (0, 128, 255), thePerson)#create rectangle in the window
+    
+    pygame.display.flip()#updates the window to every change that happens
+    
     clock.tick(20)
 
 
