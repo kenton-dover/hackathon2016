eb79075c290a46132a06607914a1f589ca584dea
ahearnkyle <ahearnkyle01@gmail.com>
2016-04-03 08:10:02 -0400

    Working game with color blocks now
    Hopefully you dont have epilepsy

--- theWindow.py
+++ theWindow.py
@@ -9,6 +9,7 @@ class goesInClientLater:
         pygame.init();
         enemyX = 0
         enemyY = 0
+        score = 0
         numberOfEnemies = 10
         battleField = 0
         arrayOfEnemies = []
@@ -37,21 +38,21 @@ class goesInClientLater:
             pressed = pygame.key.get_pressed()
             
             if pressed[pygame.K_w] and (character.y > 0):# added boundes for rectangle
-                character.y -= 3
+                character.y -= 5
                 character.yDirection = 1
                 character.xDirection = 0
 
             elif pressed[pygame.K_s]and (character.y < constants.SCREEN_HEIGHT):# bounds = screen height - rectangle height 
-                character.y += 3
+                character.y += 5
                 character.yDirection = 0
                 character.xDirection = 0
                 
             elif pressed[pygame.K_a] and (character.x > 0):
-                character.x -= 3
+                character.x -= 5
                 character.yDirection = 0
                 character.xDirection = 1
             elif pressed[pygame.K_d]and (character.x < constants.SCREEN_WIDTH):
-                character.x += 3
+                character.x += 5
                 character.yDirection = 1
                 character.xDirection = 1
             if pressed[pygame.K_SPACE]:
@@ -63,12 +64,12 @@ class goesInClientLater:
                     enemyY = random.randint(0,constants.SCREEN_HEIGHT)
                     theEnemy = pygame.Rect(enemyX,enemyY,50,50)
                     arrayOfEnemies.append(theEnemy)
-                    pygame.draw.rect(screen,(0,0,0),theEnemy)
+                    pygame.draw.rect(screen,(random.randint(0, 255),random.randint(0, 255),random.randint(0, 255)),theEnemy)
                     if arrayOfEnemies.index(theEnemy) == 9:
                         arrayIsFull = True     
             for i in range(numberOfEnemies):
                 tempEnemy = arrayOfEnemies.pop(i)
-                pygame.draw.rect(screen,(0,0,0),tempEnemy)#prints the enemies
+                pygame.draw.rect(screen,(random.randint(0, 255),random.randint(0, 255),random.randint(0, 255)),tempEnemy)#prints the enemies
                 if not character.sword.rect.colliderect(tempEnemy):
                     arrayOfEnemies.insert(i, tempEnemy)
                 else:
@@ -76,19 +77,26 @@ class goesInClientLater:
                     enemyY = random.randint(0,constants.SCREEN_HEIGHT)
                     theEnemy = pygame.Rect(enemyX,enemyY,50,50)
                     arrayOfEnemies.insert(i, theEnemy)
-                    pygame.draw.rect(screen,(0,0,0),theEnemy)               
+                    pygame.draw.rect(screen,(random.randint(0, 255),random.randint(0, 255),random.randint(0, 255)),theEnemy)
+
+                    score = score + 1
+                    pygame.display.set_caption("Score: %s" % score)
             
             for i in range(numberOfEnemies):
                 tempEnemy = arrayOfEnemies.pop(i)
                 direction = random.randint(0, 1000)
                 if direction < 250:
-                    tempEnemy = tempEnemy.move(-10,0)
+                    if tempEnemy.centerx - 10 > 0:
+                        tempEnemy = tempEnemy.move(-10,0)
                 elif direction < 500 and direction > 250:
-                    tempEnemy = tempEnemy.move(0,-10)
+                    if tempEnemy.centery - 10 > 0:
+                        tempEnemy = tempEnemy.move(0,-10)
                 elif direction < 750 and direction > 500:
-                    tempEnemy = tempEnemy.move(10,0)
+                    if tempEnemy.centerx + 10 < constants.SCREEN_WIDTH:
+                        tempEnemy = tempEnemy.move(10,0)
                 elif direction < 1000 and direction > 750:
-                    tempEnemy = tempEnemy.move(0,10)
+                    if tempEnemy.centery + 10 < constants.SCREEN_HEIGHT:
+                        tempEnemy = tempEnemy.move(0,10)
                 arrayOfEnemies.insert(i, tempEnemy)            
                 pygame.display.flip()#updates the window to every change that happens
     
